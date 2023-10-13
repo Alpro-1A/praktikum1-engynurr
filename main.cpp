ae83b3d7c4e617946c911c85ae6f361a83737218
engynurr <engygy2005@gmail.com>
2023-10-13 07:29:20 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "--------------------------------------------------------" << endl;
+    cout << "Nama: En'gy Nur Rahmadani" << endl << endl;
+    cout << "NPM : 2301631170013" << endl << endl;
+    cout << "Kelas : 1A - Informatika" << endl<< endl;
+    cout << "Cita Cita: UI/UX Designer" << endl<< endl;
+    cout << "Motivasi masuk Informatika : Nam Do San" << endl;
+    cout << "--------------------------------------------------------" << endl<<endl;
+    return 0;
+}
